9580cd2dee4f5d1537b5a3b94ef3446e83a1161e
kavyakapoor420 <kavyakapoor412@gmail.com>
2026-05-25 23:13:20 +0530

    clerk middlear tupid fix

--- client/proxy.ts
+++ client/proxy.ts
@@ -1,10 +1,13 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import {clerkMiddleware} from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+// Disable Clerk middleware for this deployment.
+export default function middleware(req: Request) {
+    return fetch(req);
+}
 
 export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
-  ],
+    matcher: [
+        "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+        "/(api|trpc)(.*)",
+    ],
 };
